231818ad4810381d7f4bce5e8d1ec4779d0fa321
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-03 12:07:07 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get install docker.io
+apt-get install docker.io
 
-sudo systemctl start docker
+systemctl start docker
 
-sudo docker login -u muthummkdh -p mmk07081999
+docker login -u muthummkdh -p mmk07081999
 
-sudo docker run -itd --name myapp -p "9090:80" new:1.1
+docker run -itd --name myapp -p "9090:80" new:1.1
 
 echo "deploy success"
