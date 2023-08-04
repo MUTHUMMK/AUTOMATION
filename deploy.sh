bde4294b304dd81b140a631868cf7fc8588f81ee
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-04 16:29:44 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-set -e
-
+export pwd="1$"
+sh "ssh -o StrictHostKeyChecking=no -i "$pwd" ubuntu@13.215.59.213 << EOF
 apt-get update
 
 apt-get install docker.io
@@ -13,3 +12,4 @@ docker login -u muthummkdh -p mmk07081999
 docker run -itd --name myapp -p "9090:80" new:1.1
 
 echo "deploy success"
+EOF
