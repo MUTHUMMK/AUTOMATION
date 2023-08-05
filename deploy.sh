b3e98ff0aeeff387bec0440b3746fcf3c3662991
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-05 17:24:44 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 export pwd="$1"
-ssh -o StrictHostKeyChecking=no -i "$pwd" ubuntu@13.215.59.213 <<EOF
+ssh -o StrictHostKeyChecking=no -i "$pwd" ubuntu@13.213.65.61 <<EOF
 sudo apt-get update -y
 ls
 sudo apt-get install docker.io -y
 
 sudo systemctl start docker
 
-sudo docker login -u muthummkdh -p mmk07081999
-
-sudo docker run -itd --name myapp -p "9090:80" muthummkdh/new1
-
-if curl localhost:9090
+if curl localhost:80
 then
   echo "deploy successed"
 fi
