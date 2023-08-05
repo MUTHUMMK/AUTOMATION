f27eb3b366cbb1528cc0ce7331d2d75b12eb1676
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-05 17:01:36 +0530

    build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -e
-docker tag muthu:1.1 muthummkdh/new3
-docker push muthummkdh/new3
+
+docker build -t muthu:1.1 .
+
+docker login -u DOCKER_HUB_USR -p DOCKER_HUB_PSW
+
+docker tag muthu:1.1 muthummkdh/new2
+docker push muthummkdh/new2
