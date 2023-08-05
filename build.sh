d652eba4937528f5884b81af5bd174832fcdd823
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-05 14:37:01 +0530

    build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 set -e
-
-export user = "Dockehub_user"
-export pwd = "Dockerhub_pwd"
-
-docker build -t muthu:1.1 .
-docker login -u "$user" -p "$pwd"
 docker tag muthu:1.1 muthummkdh/new3
 docker push muthummkdh/new3
