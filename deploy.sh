f403266ee0d48cc4515969262b1ff74c2d698c19
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-05 17:44:15 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,14 @@ export pwd="$1"
 
 scp -o StrictHostKeyChecking=no -i "$pwd" docker-compose.yml ubuntu@13.213.65.61:/home/ubuntu
 ssh -o StrictHostKeyChecking=no -i "$pwd" ubuntu@13.213.65.61 <<EOF
-sudo apt-get update -y
+apt-get update -y
 ls
 
-sudo apt-get install docker.io -y
-sudo  apt-get install docker-compose -y
+apt-get install docker.io -y
+apt-get install docker-compose -y
 
-sudo systemctl start docker
-
-
-sudo docker-compose up -d
+systemctl start docker
+docker-compose up -d
 
 if curl localhost:80
 then
