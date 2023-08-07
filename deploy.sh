c5e0f79b541579bc6a44b042f014554c9d59c862
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-07 08:33:18 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Terraform use to create the infrastructure for instances
-terraform init && terraform plan && terraform apply -auto-approve
+terraform init && terraform apply -auto-approve
 
 # to get public IP and store the variable 
 a=$(aws ec2 describe-instances --region ap-southeast-1 --filters 'Name=tag:Name,Values=MMK' --query 'Reservations[].Instances[].PublicIpAddress' --output text)
@@ -9,8 +9,8 @@ a=$(aws ec2 describe-instances --region ap-southeast-1 --filters 'Name=tag:Name,
 echo $a
 
 # login the ssh-remote server & put the variable instead of public ip
-scp -o StrictHostKeyChecking=no -i "$sshkey" docker-compose.yml ubuntu@$a:/home/ubuntu
-ssh -o StrictHostKeyChecking=no -i "$sshkey" ubuntu@$a<<EOF
+sudo scp -o StrictHostKeyChecking=no -i $sshkey docker-compose.yml ubuntu@$a:/home/ubuntu
+sudo ssh -o StrictHostKeyChecking=no -i $sshkey ubuntu@$a<<EOF
 sudo apt-get update -y
 ls
 
